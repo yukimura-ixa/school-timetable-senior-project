fe985e52f2f95a3aca10f29f683a6181077a938f
Napat Phobutdee <pbd.napat1111@gmail.com>
2023-10-16 01:49:42 +0700

    route teacher

---
+++ src/app/management/teacher/api/route.ts
@@ -0,0 +1,20 @@
+import mysql from "mysql2";
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "admin",
+  database: "school_timetable",
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+export async function GET(request: Request) {
+  const [data, f] = await pool.promise().query("SELECT * FROM `teacher`");
+
+  return Response.json({ data });
+}
