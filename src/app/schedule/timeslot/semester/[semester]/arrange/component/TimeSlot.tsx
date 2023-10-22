d95ed3aaff1871e0d4c990877565d76a3c24de87
oberghub <nataponball@hotmail.com>
2023-10-22 13:18:39 +0700

    timeslot mockup (can't interaction)

--- src/app/schedule/timeslot/semester/[semester]/arrange/component/TimeSlot.tsx
+++ src/app/schedule/timeslot/semester/[semester]/arrange/component/TimeSlot.tsx
@@ -1,40 +1,87 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-type Props = {}
+type Props = {};
 
 function TimeSlot(props: Props) {
+  const timsSlotData = {
+    SlotAmount: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    StartTime: "",
+    Duration: 50,
+    DayOfWeek: ["จันทร์", "อังคาร", "พุธ", "พฤหัสบดี", "ศุกร์"],
+    BreakSlot: [4, 5],
+  };
   return (
     <>
-    <div className='flex flex-col gap-3 w-full items-center'>
+      <div className="flex flex-col gap-3 w-full">
         {/* top row */}
-        <div className='flex gap-4 justify-around items-center'>
-            <div className='flex w-auto p-4 h-[53px] rounded border border-[#ABBAC1] items-center justify-center'>
-                <p className=''>คาบที่</p>
-            </div>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-                <Fragment key={`woohoo${item}`}>
-                    <div className='flex p-4 w-auto h-[53px] rounded border border-[#ABBAC1] items-center justify-center'>
-                        <p className=''>{item}</p>
-                    </div>
-                </Fragment>
-            ))}
+        <div className="flex grow gap-4 items-center">
+          <div className="flex items-center w-[85px] justify-center p-[10px] h-[53px] rounded border border-[#ABBAC1]">
+            <div className="flex w-[50px] h-[24px] justify-center">คาบที่</div>
+          </div>
+          {timsSlotData.SlotAmount.map((item) => (
+            <Fragment key={`woohoo${item}`}>
+              <div className="flex grow items-center justify-center p-[10px] h-[53px] rounded border border-[#ABBAC1]">
+                <p className="">{item}</p>
+              </div>
+            </Fragment>
+          ))}
         </div>
         {/* time row */}
-        {/* <div className='flex gap-4 justify-around items-center'>
-            <div className='flex p-4 h-[40px] rounded border border-[#ABBAC1] items-center justify-center'>
-                <p className=''>เวลา</p>
-            </div>
-            {["8.30-9.20", "9.20-10.10", "8.30-9.20", "8.30-9.20", "8.30-9.20", "8.30-9.20", "8.30-9.20", "8.30-9.20", "8.30-9.20", "8.30-9.20"].map((item) => (
+        <div className="flex grow gap-4 items-center">
+          <div className="flex items-center w-[85px] justify-center p-[10px] h-[40px] rounded border border-[#ABBAC1]">
+            <div className="flex w-[50px] h-[24px] justify-center">เวลา</div>
+          </div>
+          {[
+            "08.30-09.20",
+            "09.20-10.10",
+            "10.10-11.00",
+            "11.00-11.50",
+            "11.50-12.40",
+            "12.40-13.30",
+            "13.30-14.20",
+            "14.20-15.10",
+            "15.10-16.00",
+            "16.00-16.50",
+          ].map((item) => (
+            <Fragment key={`woohoo${item}`}>
+              <div className="flex grow items-center justify-center py-[10px] h-[40px] rounded border border-[#ABBAC1]">
+                <p className="flex text-xs w-full items-center justify-center h-[24px]">
+                  {item}
+                </p>
+              </div>
+            </Fragment>
+          ))}
+        </div>
+        {/* day */}
+        {timsSlotData.DayOfWeek.map((day) => (
+          <Fragment key={`asdasda${day}`}>
+            <div className="flex grow gap-4 items-center">
+              <div className="flex items-center w-[85px] justify-center p-[10px] h-[76px] rounded border border-[#ABBAC1]">
+                <div className="flex w-[50px] h-[24px] justify-center">
+                  {day}
+                </div>
+              </div>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
                 <Fragment key={`woohoo${item}`}>
-                    <div className='flex h-[40px] rounded border border-[#ABBAC1] items-center justify-center'>
-                        <p className='text-xs'>{item}</p>
-                    </div>
+                  <div className="flex grow items-center cursor-pointer justify-center p-[10px] h-[76px] rounded border border-[#ABBAC1]">
+                    {timsSlotData.BreakSlot.includes(item) ? (
+                      <div className="flex flex-col items-center w-14 "></div>
+                    ) : (
+                      <div className="flex flex-col items-center w-14 ">
+                        <p className="text-xs">ท21102</p>
+                        <p className="text-xs">2/1</p>
+                        <p className="text-xs">ห้อง 325</p>
+                      </div>
+                    )}
+                  </div>
                 </Fragment>
-            ))}
-        </div> */}
-    </div>
+              ))}
+            </div>
+          </Fragment>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default TimeSlot
+export default TimeSlot;
