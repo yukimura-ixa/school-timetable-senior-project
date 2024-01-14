23f3101e64fa4c1f820a51533b2fc3dc918dc786
Napat Phobutdee <pbd.napat1111@gmail.com>
2024-01-14 21:35:29 +0700

    timeslot break both

--- src/app/api/timeslot/route.ts
+++ src/app/api/timeslot/route.ts
@@ -19,26 +19,46 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
-    // body: { TimeslotPerDay, AcademicYear, Semester, Duration, Breaktime, Days}
+    // body: { TimeslotPerDay, AcademicYear, Semester, Duration, BreakTimeslots, Days, StartTime}
     try {
         const body = await request.json()
         console.log(body)
-        const data = await Promise.all(
-            Array.from({ days: body.Days }, (day: day_of_week, index) =>
-                Array.from({ length: body.TimeslotPerDay }, (_, index) =>
-                    prisma.timeslot.create({
-                        data: {
-                            TimeslotID: body.Semester + '/' + body.AcademicYear + '-' + day_of_week[day] + (index + 1),
-                            DayOfWeek: day_of_week[day],
-                            AcademicYear: body.AcademicYear,
-                            Semester: body.Semester,
-                            StartTime: body.StartTime,
-                            EndTime: body.EndTime,
-                            Breaktime: breaktime[body.Breaktime],
-                        },
-                    })
-                )
-            ))
+
+        const timeslots: timeslot[] = []
+        for (let day of body.Days) {
+            for (let index = 0; index < body.TimeslotPerDay; index++) {
+                let startTime = new Date(`1970-01-01T${body.StartTime}:00Z`)
+                startTime.setMinutes(startTime.getMinutes() + (index + 1) * body.Duration)
+
+                let endTime = new Date(startTime)
+                endTime.setMinutes(endTime.getMinutes() + body.Duration)
+
+                let isBreak: breaktime
+                if (body.BreakTimeslots.Junior === index + 1 && body.BreakTimeslots.Senior === index + 1) {
+                    isBreak = breaktime["BREAK_BOTH"]
+                } else if (body.BreakTimeslots.Senior === index + 1) {
+                    isBreak = breaktime["BREAK_SENIOR"]
+                } else if (body.BreakTimeslots.Junior === index + 1) {
+                    isBreak = breaktime["BREAK_JUNIOR"]
+                } else {
+                    isBreak = breaktime["NOT_BREAK"]
+                }
+
+                timeslots.push({
+                    TimeslotID: body.Semester[9] + '/' + body.AcademicYear + '-' + day_of_week[day] + (index + 1),
+                    DayOfWeek: day_of_week[day],
+                    AcademicYear: body.AcademicYear,
+                    Semester: body.Semester,
+                    StartTime: startTime,
+                    EndTime: endTime,
+                    Breaktime: isBreak,
+                })
+            }
+        }
+
+        const data = await prisma.timeslot.createMany({
+            data: timeslots,
+        })
 
         return NextResponse.json(data)
     } catch (error) {
@@ -46,6 +66,7 @@ export async function POST(request: NextRequest) {
         return NextResponse.error()
     }
 }
+
 
 export async function PUT(request: NextRequest) {
     // body: { TimeslotPerDay, AcademicYear, Semester, StartTime, EndTime, Breaktime }
