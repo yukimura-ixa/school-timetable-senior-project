bf13c97829fa4570b0fb4f9d96273146031419e2
Napat Phobutdee <pbd.napat1111@gmail.com>
2024-01-28 13:19:05 +0700

    Update route.ts

--- src/app/api/assign/route.ts
+++ src/app/api/assign/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/libs/prisma"
 import { NextRequest, NextResponse } from "next/server"
-import { teacher, teachers_responsibility, subject, semester } from "@prisma/client"
-import { subject_credit, day_of_week } from "@prisma/client"
+import { teachers_responsibility, semester } from "@prisma/client"
+import { subjectCreditValues } from "@/models/credit-value"
 
 export async function GET(request: NextRequest) {
   // localhost:3000/api/assign?TeacherID=1&Semester=SEMESTER_1&AcademicYear=2566
@@ -21,18 +21,10 @@ export async function GET(request: NextRequest) {
         teacher: true,
       },
     })
-    // const data = await prisma.teachers_responsibility.groupBy({
-    //   by: ["SubjectCode", "GradeID", "TeachHour"],
-    //   _sum: {
-    //     TeachHour: true,
-    //   },
-    //   where: {
-    //     TeacherID: TeacherID,
-    //     AcademicYear: AcademicYear,
-    //     Semester: Semester,
-    //   }
-    // })
-    return NextResponse.json(data)
+
+
+    const results = data.map((resp) => { return { ...resp, SubjectName: resp.subject.SubjectName, Credit: resp.subject.Credit } })
+    return NextResponse.json(results)
   } catch (error) {
     console.log(error)
     return NextResponse.json({ error: error }, { status: 500 })
@@ -43,59 +35,24 @@ export async function POST(request: NextRequest) {
   // body: {  TeacherID, GradeID, SubjectCode, AcademicYear, Semester, TeachHour}
   try {
     const body = await request.json()
-    const data = await prisma.teachers_responsibility.create({
-      data: {
+    const results = []
+
+    // Retrieve existing responsibilities from the database
+    const existingResponsibilities = await prisma.teachers_responsibility.findMany({
+      where: {
+        TeacherID: body.TeacherID,
         AcademicYear: body.AcademicYear,
         Semester: body.Semester,
-        TeachHour: body.TeachHour,
-        teacher: {
-          connect: {
-            TeacherID: body.TeacherID,
-          },
-        },
-        subject: {
-          connect: {
-            SubjectCode: body.SubjectCode,
-          },
-        },
-        gradelevel: {
-          connect: {
-            GradeID: body.GradeID,
-          },
-        },
-      }
+      },
+      include: {
+        subject: true,
+      },
     })
-    return NextResponse.json(data)
+
+    return NextResponse.json({ status: "success", results: results })
   } catch (error) {
     console.log(error)
     return NextResponse.json({ error: error }, { status: 500 })
   }
 }
 
-export async function PUT(request: NextRequest) {
-  // body: { TeacherID, Prefix, Firstname, Lastname, Department }
-  try {
-    const body = await request.json()
-    const data = await Promise.all(
-      body.map(async (element) => {
-        return prisma.teacher.update({
-          where: {
-            TeacherID: element.TeacherID,
-          },
-          data: {
-            Prefix: element.Prefix,
-            Firstname: element.Firstname,
-            Lastname: element.Lastname,
-            Department: element.Department,
-          },
-        })
-      })
-    )
-    const ids = data.map((record) => record.id)
-
-    return NextResponse.json(ids)
-  } catch (error) {
-    console.log(error)
-    return NextResponse.json({ error: error }, { status: 500 })
-  }
-}
