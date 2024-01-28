24f03cb58867aaac5d9bd98fe405fc3234fecca3
Napat Phobutdee <pbd.napat1111@gmail.com>
2024-01-28 14:45:01 +0700

    class api GET

---
+++ src/app/api/class/route.ts
@@ -0,0 +1,61 @@
+import prisma from "@/libs/prisma"
+import { semester, type class_schedule } from "@prisma/client"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(request: NextRequest) {
+
+    const AcademicYear = parseInt(request.nextUrl.searchParams.get("AcademicYear"))
+    const Semester = semester[request.nextUrl.searchParams.get("Semester")]
+    let response: class_schedule[]
+    try {
+        if (request.nextUrl.searchParams.has("TeacherID")) {
+            // localhost:3000/api/class?AcademicYear=2566&Semester=SEMESTER_1&TeacherID=1
+            const TeacherID = parseInt(request.nextUrl.searchParams.get("TeacherID"))
+            response = await prisma.class_schedule.findMany({
+                where: {
+                    timeslot: {
+                        AcademicYear: AcademicYear,
+                        Semester: Semester
+                    },
+                    subject: {
+                        teachers_responsibility: {
+                            every: {
+                                TeacherID: TeacherID
+                            }
+                        }
+                    }
+                },
+                include: {
+                    subject: true,
+                    gradelevel: true,
+                    timeslot: true,
+                    room: true,
+                },
+
+            })
+        } else if (request.nextUrl.searchParams.has("GradeID")) {
+            // localhost:3000/api/class?AcademicYear=2566&Semester=SEMESTER_1&GradeID=101
+            const GradeID = request.nextUrl.searchParams.get("GradeID")
+            response = await prisma.class_schedule.findMany({
+                where: {
+                    timeslot: {
+                        AcademicYear: AcademicYear,
+                        Semester: Semester
+                    },
+                    GradeID: GradeID
+                },
+                include: {
+                    subject: true,
+                    gradelevel: true,
+                    timeslot: true,
+                    room: true,
+                },
+
+            })
+        }
+        return NextResponse.json(response)
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+}
