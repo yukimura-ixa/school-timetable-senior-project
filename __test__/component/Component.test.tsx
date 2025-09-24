8d5b04353ff5cb89c3f35e06cfcbb5e72aca14fd
yukimura-ixa <61940400+yukimura-ixa@users.noreply.github.com>
2025-09-24 20:32:13 +0700

    Fix hex color conversion and update tests

--- __test__/component/Component.test.tsx
+++ __test__/component/Component.test.tsx
@@ -1,18 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import Button from '@/app/components/elements/static/Button';
-import adduserIcon from '../public/adduser.svg';
+import { fireEvent, render, screen } from '@testing-library/react'
+import Button from '@/components/elements/static/Button'
 
-test('render button tests', () => {
-    render(<Button
-            title={'My button'}
-         />);
-    const getTitle = screen.getByText('My button');
-    expect(getTitle).toBeInTheDocument();
-})
-test('find alt text of button tests', () => {
-    render(<Button
-            icon={adduserIcon}
-         />);
-    const altText = screen.getByAltText('icon')
-    expect(altText).toBeInTheDocument();
+describe('Button component', () => {
+  it('renders provided title', () => {
+    render(<Button icon="/icon.svg" title="My button" />)
+
+    expect(screen.getByText('My button')).toBeInTheDocument()
+  })
+
+  it('displays icon alt text when icon prop is provided', () => {
+    render(<Button icon="/icon.svg" />)
+
+    expect(screen.getByAltText('icon')).toBeInTheDocument()
+  })
+
+  it('calls the provided click handler when clicked', () => {
+    const handleClick = jest.fn()
+    render(<Button icon="/icon.svg" title="Clickable" handleClick={handleClick} />)
+
+    fireEvent.click(screen.getByRole('button', { name: /Clickable/ }))
+
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
 })
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom/extend-expect'
+import React from 'react'
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: React.forwardRef((props, ref) =>
+    React.createElement('img', { ...props, ref }),
+  ),
+}))
