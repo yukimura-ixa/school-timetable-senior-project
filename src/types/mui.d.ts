7a37a86d137d6406855c05230b1f0216cd6b04e1
Napat Phobutdee <61940400+yukimura-ixa@users.noreply.github.com>
2025-12-23 21:38:16 +0700

    fix: extend mui palette types

--- src/types/mui.d.ts
+++ src/types/mui.d.ts
@@ -1,5 +1,26 @@
 import "@mui/material/styles";
 
+type SlatePalette = {
+  50: string;
+  100: string;
+  200: string;
+  300: string;
+  400: string;
+  500: string;
+  600: string;
+  700: string;
+  800: string;
+  900: string;
+};
+
+type GlassPalette = {
+  main: string;
+  light: string;
+  dark: string;
+  border: string;
+  borderLight: string;
+};
+
 declare module "@mui/material/styles" {
   interface PaletteColor {
     lighter?: string;
@@ -7,5 +28,15 @@ declare module "@mui/material/styles" {
 
   interface SimplePaletteColorOptions {
     lighter?: string;
+  }
+
+  interface Palette {
+    slate: SlatePalette;
+    glass: GlassPalette;
+  }
+
+  interface PaletteOptions {
+    slate?: SlatePalette;
+    glass?: GlassPalette;
   }
 }
